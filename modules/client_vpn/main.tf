4eddc1ef8fefe877d5e06e8668f22993f271205c
mirinus <mirinus@gmail.com>
2026-01-12 20:52:58 +0900

    Fix Client VPN route configuration

--- modules/client_vpn/main.tf
+++ modules/client_vpn/main.tf
@@ -119,7 +119,7 @@ resource "aws_ec2_client_vpn_route" "spoke_vpcs" {
   for_each               = toset(var.spoke_vpc_cidrs_list)
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.keycloak.id
   destination_cidr_block = each.value
-  target_vpc_subnet_id   = var.hub_secondary_subnet_id
+  target_vpc_subnet_id   = var.hub_primary_subnet_id
   description            = "Route to Spoke VPC ${each.value} via TGW"
   
   depends_on = [
